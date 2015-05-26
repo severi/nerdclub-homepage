b61ff055861bf6e32010b41a36f1e7c5506d11e7
Severi <severi.haverila@gmail.com>
2015-05-27 00:12:03 +0200

    removed warning

--- public/modules/calendarevents/services/geolocation.client.service.js
+++ public/modules/calendarevents/services/geolocation.client.service.js
@@ -8,7 +8,6 @@ angular.module('calendarevents').factory('Geolocation', ['$http',
         // Public API
         return {
             getGeolocation: function(address) {
-                console.log(address+" <- address");
                 return $http.get('http://nominatim.openstreetmap.org/search/'+address+'?format=json&addressdetails=1&limit=1&polygon_svg=1');
             }
         };
