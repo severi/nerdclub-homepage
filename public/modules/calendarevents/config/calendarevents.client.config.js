24c710e07e752ecc2bef1ceb5be45f873b6fa827
Severi <severi.haverila@gmail.com>
2015-08-05 22:18:58 +0300

    calEvent menuitem modified

--- public/modules/calendarevents/config/calendarevents.client.config.js
+++ public/modules/calendarevents/config/calendarevents.client.config.js
@@ -3,9 +3,7 @@
 // Configuring the Articles module
 angular.module('calendarevents').run(['Menus',
 	function(Menus) {
-		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Calendarevents', 'calendarevents', 'dropdown', '/calendarevents(/create)?');
-		Menus.addSubMenuItem('topbar', 'calendarevents', 'List Calendarevents', 'calendarevents');
-		Menus.addSubMenuItem('topbar', 'calendarevents', 'New Calendarevent', 'calendarevents/create');
+	    // Set top bar menu items
+            Menus.addMenuItem('topbar', 'Calendarevents', 'calendarevents', 'item');
 	}
 ]);
